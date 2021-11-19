3778d5e0ba70378356ffb2168fa28e7ec431cd97
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-11-19 14:45:00 -0600

    exercise 6

---
+++ sales_agent_invoices.sql
@@ -0,0 +1,6 @@
+SELECT Invoice.InvoiceId, Employee.FirstName, Employee.LastName
+FROM Invoice    
+    JOIN Customer
+        ON Invoice.CustomerId = Customer.CustomerId
+    JOIN Employee
+        ON Customer.SupportRepId = Employee.EmployeeId
